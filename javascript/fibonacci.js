94863bdbe7fcf692e6f9fc4d4819986ea08194d6
Wilfred <wilfredbayudan@gmail.com>
2021-10-07 22:11:06 -1000

    commit tree f54d42800a231196dd11455101f79fdf4b39d05d

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-  // type your code here
+
+  if (num < 2) {
+    return num;
+  }
+
+  const sequence = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    const seqLength = sequence.length;
+    sequence.push(sequence[seqLength -1] + sequence[seqLength -2]);
+  }
+
+  return sequence.pop();
+
 }
 
 if (require.main === module) {
@@ -16,9 +29,66 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("");
+
+  console.log("Expecting: 89");
+  console.log("=>", fibonacci(11));
 }
 
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+
+Find the "x"nth element in the Fibonaci series
+
+Fibonacci sequence starts with 0, followed by a 1. All numbers after are the sum of the 2 numbers preceeding it in the sequence.
+
+0 1 2 3 4 5 6 7  8  9  10 11 <--- Using zero-indexing
+0 1 1 2 3 5 8 13 21 34 55 89 etc etc
+    ^ ^ ^
+0+1=1 ^ ^
+  1+1=2 ^
+    1+2=3
+
+  if....
+  6
+  build fib sequence till 5th index
+  
+  create array with [0,1]
+
+    check (if array.length === num+1) return array[num]
+
+    0+1 = 1, push to array
+
+    [0,1,1]
+
+    1+1 = 2, push to array
+
+    [0,1,1,2]
+
+    1+2 = 3, push to array
+
+    [0,1,1,2,3]
+
+    2+3 = 5, push to array
+
+    [0,1,1,2,3,5]
+
+
+  function called fibonacci takes integer "num"
+
+    if number is less than 2
+      return num
+
+    create array called sequence with [0, 1] (first two numbers of sequence)
+
+    
+
+
+
+
+
+*/
